17eea3db9afeb9a1db666b18d35b74b61f7ff389
Murari007 <gmurari911@gmail.com>
2025-10-09 15:58:33 +0530

    Added serialiation of encodings and fixed deserialization issues

--- compiler/src/iree/compiler/Codegen/ExternalInterfaces/CPUEncodingExternalModels.cpp
+++ compiler/src/iree/compiler/Codegen/ExternalInterfaces/CPUEncodingExternalModels.cpp
@@ -1180,7 +1180,6 @@ struct Exsleratev2LayoutResolverAttr final
                                       DictionaryAttr config) const {
     MLIRContext *ctx = attr.getContext();
     SmallVector<NamedAttribute> configItems;
-    storeNamedAttrIfPresent(configItems, config, "ukernels");
     return Exsleratev2EncodingResolverAttr::get(
         ctx, DictionaryAttr::get(ctx, configItems));
   }
@@ -1195,6 +1194,13 @@ struct Exsleratev2LayoutResolverAttr final
 struct Exsleratev2SerializableAttr final
     : IREE::Encoding::SerializableAttr::ExternalModel<
           Exsleratev2SerializableAttr, Exsleratev2EncodingResolverAttr> {
+
+  bool isSerialized(Attribute attr) const {
+    auto configuration =
+        cast<Exsleratev2EncodingResolverAttr>(attr).getConfiguration();
+    return configuration && configuration.contains(kEncodingInfoAttrName);
+  }
+
   Value calculateStorageSizeInBytes(Attribute attr, Location loc,
                                     OpBuilder &builder, RankedTensorType type,
                                     ValueRange dynamicDims) const {
